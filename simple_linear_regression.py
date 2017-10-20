fdb3dad6a94b2a418c19c9f3efd6d9a66a44d61c
RavneetDTU <ravneet.dtu@Gmail.com>
2017-10-20 22:51:50 +0530

    Simple Linear Regression Applied to Salary Data Using Internal Library

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -5,3 +5,34 @@ Created on Fri Oct 20 20:43:24 2017
 @author: Ravneet
 """
 
+#Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting Simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+# Predicting the Test Set Results
+y_pred = regressor.predict(X_test)
